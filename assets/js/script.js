433d25050859aef278ded984d134b822b5681623
alincoleo <alvaro.lincoleo@gmail.com>
2022-04-12 19:10:50 -0400

    Esqueleto, bases y cdn

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// funcion padre que asegura primero se carguen la página
+$(document).ready(function () {
+    // funciones hijas
+})
+// asegurando funcionalidad
+// poppover
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+// tooltip
+var tooltipTriggerList =
+[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+return new bootstrap.Tooltip(tooltipTriggerEl)
+})
