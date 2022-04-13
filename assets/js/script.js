6dbafed70d5da07659fea21a76232904fcff8a57
alincoleo <alvaro.lincoleo@gmail.com>
2022-04-13 14:52:11 -0400

    responsivo, modal, smoth scroll, tooltip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
 // funcion padre que asegura primero se carguen la página
 $(document).ready(function () {
-    // funciones hijas
+  // funciones hijas
+  $(".nav-link").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html').animate({
+        scrollTop: $(hash).offset().top
+      }, 2000, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
 })
 // asegurando funcionalidad
 // poppover
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
+// var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+// var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+//   return new bootstrap.Popover(popoverTriggerEl)
+// })
 // tooltip
 var tooltipTriggerList =
-[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-return new bootstrap.Tooltip(tooltipTriggerEl)
+  return new bootstrap.Tooltip(tooltipTriggerEl)
 })
